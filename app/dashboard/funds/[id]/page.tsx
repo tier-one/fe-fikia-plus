92ac4a0d7622ff211dddfa9af9a6ee3b26425c91
ngirimana <chadrackngirimana@gmail.com>
2023-06-06 20:39:24 +0200

    width fix

--- app/dashboard/funds/[id]/page.tsx
+++ app/dashboard/funds/[id]/page.tsx
@@ -5,10 +5,6 @@ import Link from 'next/link';
 import Image from "next/image"
 import Card from "@/app/components/Card";
 import Table from '@/app/components/Table';
-import Modal from "@/app/components/Modal";
-import InputField from "@/app/components/InputField";
-import Button from "@/app/components/Button";
-import SelectBox from "@/app/components/SelectBox";
 import Settings from "@/app/components/Settings";
 
 const inputFieldStylingProps = {
@@ -52,7 +48,6 @@ const data = [
 ]
 export default function Dashboard() {
 
-    const [isOpen, setIsOpen] = useState(false);
     const [fundName, setFundName] = useState('')
     const [fundType, setFundType] = useState('')
     const [activeTab, setActiveTab] = useState("fund-transactions");
@@ -61,26 +56,9 @@ export default function Dashboard() {
         setActiveTab(tab);
     };
 
-    const handleFundName = (fundName: string) => {
-        setFundName(fundName)
-    }
-    const handleFundType = (fundType: string) => {
-        setFundType(fundType)
-    }
-    const openModal = () => {
-        setIsOpen(true)
-    }
-    const closeModal = () => {
-        setIsOpen(false)
-    }
-    const fundTypes: string[] = [
-        "Bk fund",
-        "Equity Fund"
-    ]
-
     return (
         <div className="bg-[#eaeaed] min-h-[87vh] ">
-            <div className='flex flex-col justify-center lg:mx-10'>
+            <div className='flex flex-col  lg:mx-[15rem] '>
                 <div className="flex mt-3">
                     <Link href="/dashboard/funds" className="mr-3 text-[#475569] font-semibold">
                         Funds
@@ -91,7 +69,7 @@ export default function Dashboard() {
                 <div>
                     <p className="text-[#475569] font-bold text-2xl py-4">Funds</p>
                 </div>
-                <div className="border-b border-gray-300 mb-4  ">
+                <div className="border-b border-gray-300 mb-4">
                     <button className={`py-2 text-xl text-center border-b-2 font-medium text-[#475569] ${activeTab === "fund-transactions" ? 'border-[#0EA4E8]' : ''} leading-5 focus:outline-none focus:border-[#0EA4E8]`} onClick={() => handleTabClick("fund-transactions")}>
                         Fund transactions
                     </button>
@@ -101,8 +79,8 @@ export default function Dashboard() {
                     </button>
                 </div>
                 {activeTab === "fund-transactions" ?
-                    <div className="flex flex-col">
-                        <div className="flex lg:justify-between flex-col lg:flex-row flex-wrap">
+                    (<>
+                        <div className="flex-grow flex lg:justify-between flex-col lg:flex-row" >
                             <Card
                                 title="Total Clients"
                                 imageAlt="Total Clients"
@@ -142,45 +120,12 @@ export default function Dashboard() {
                                 title="Transactions"
                                 buttonText="Create fund"
                                 buttonStyling="bg-[#002674] text-white py-2 px-4 mt-2 ml-4 rounded-lg"
-                                buttonOnClick={openModal}
+                                buttonOnClick={()=>{}}
                                 itemsPerPage={7}
                                 displayButton={false}
                             />
                         </div>
-                        <Modal isOpen={isOpen} onClose={closeModal} >
-                            <div className='bg-white rounded-lg  h-1/1 px-10 py-5'>
-                                <p className="px-8 text-[#475569] font-semibold">Create Fund</p>
-                                <div className='py-3'>
-                                    <InputField
-                                        value={fundName}
-                                        placeholder='Enter Fund name'
-                                        required={false}
-                                        type='text'
-                                        className='text-xs'
-                                        label='Fund name'
-                                        onChange={handleFundName}
-                                        {...inputFieldStylingProps}
-                                    />
-                                </div>
-                                <div className='py-3'>
-                                    <SelectBox
-                                        value={fundType}
-                                        required={false}
-                                        values={fundTypes}
-                                        className='text-xs'
-                                        label='Fund type'
-                                        onChange={handleFundType}
-                                        {...inputFieldStylingProps}
-                                    />
-                                </div>
-                                <div className="flex justify-end px-8 ">
-                                    <Button styling='bg-[#002674] text-white py-2 px-4 mt-2  rounded-lg ' value='Create fund' />
-                                    <Button styling='bg-[#F0F4F8] text-[#475569] py-2 px-4 mt-2 ml-4 rounded-lg ' value='Cancel' />
-                                </div>
-                            </div>
-                        </Modal>
-                    </div> : <Settings />}
-
+                    </> ): <Settings />}
             </div>
         </div>
     );
