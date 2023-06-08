057adaf09e6273016206c944c67f5ac1c96c128f
ngirimana <chadrackngirimana@gmail.com>
2023-06-08 23:09:51 +0200

    clients page ui

--- app/dashboard/clients/page.tsx
+++ app/dashboard/clients/page.tsx
@@ -1,7 +1,129 @@
-export default function Cleints() {
+"use client";
+
+import { useState } from "react";
+import Table from "@/app/components/Table";
+import Modal from "@/app/components/Modal";
+import InputField from "@/app/components/InputField";
+import Button from "@/app/components/Button";
+import SelectBox from "@/app/components/SelectBox";
+
+const inputFieldStylingProps = {
+  container: {
+    className: "flex flex-col space w-full px-8",
+  },
+  inputlabel: {
+    className: "text-base text-gray-600 font-light",
+  },
+  input: {
+    className:
+      "py-3 px-5 rounded-lg mt-2 border border-gray-300 placeholder:text-gray-600",
+  },
+};
+
+export default function Funds() {
+  const headers = ["No", "Fund name", "Unit Price", "24h%", "Market Cap"];
+
+  const data = [
+    {
+      No: 1,
+      name: "Unguka fund",
+      unitPrice: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 2,
+      name: "Terimbere fund",
+      fund: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 3,
+      name: "Umwalimu fund",
+      fund: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 4,
+      name: "Ejo heza fund",
+      fund: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 5,
+      name: "Ejo heza fund",
+      fund: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 6,
+      name: "Bk fund",
+      fund: 3000,
+      change: -3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 7,
+      name: "Equity fund",
+      fund: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 8,
+      name: "I&M fund",
+      fund: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 9,
+      name: "KCB fund",
+      fund: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 10,
+      name: "Access fund",
+      fund: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+    {
+      No: 11,
+      name: "RIM fund",
+      fund: 3000,
+      change: 3.3,
+      marketCap: 345455656.34,
+    },
+  ];
+
   return (
-    <div>
-      Clients
+    <div className="bg-[#eaeaed] min-h-[87vh] ">
+      <div className=" flex flex-col justify-center lg:mx-[9rem]">
+        <p className="text-[#475569] font-bold text-3xl py-3">Clients</p>
+        <div className="flex flex-grow min-h-[66vh]">
+          <Table
+            headers={headers}
+            data={data}
+            title="Client history"
+            buttonText=""
+            buttonStyling="bg-[#002674] text-white py-2 px-4 mt-2 ml-4 rounded-lg"
+            buttonOnClick={() => {}}
+            marketCapIndex={4}
+            changeIndex={3}
+            itemsPerPage={7}
+            idIndex={0}
+            isTableRowLink={false}
+            displayButton={false}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- app/dashboard/funds/page.tsx
+++ app/dashboard/funds/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
 import { useState } from "react";
-import Table from '@/app/components/Table';
+import Table from "@/app/components/Table";
 import Modal from "@/app/components/Modal";
 import InputField from "@/app/components/InputField";
 import Button from "@/app/components/Button";
@@ -9,129 +9,103 @@ import SelectBox from "@/app/components/SelectBox";
 
 const inputFieldStylingProps = {
   container: {
-    className: 'flex flex-col space w-full px-8'
+    className: "flex flex-col space w-full px-8",
   },
   inputlabel: {
-    className: 'text-base text-gray-600 font-light'
+    className: "text-base text-gray-600 font-light",
   },
   input: {
-    className: 'py-3 px-5 rounded-lg mt-2 border border-gray-300 placeholder:text-gray-600'
+    className:
+      "py-3 px-5 rounded-lg mt-2 border border-gray-300 placeholder:text-gray-600",
   },
-}
-
-
+};
 
 export default function Funds() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [fundName, setFundName] = useState('')
-  const [fundType, setFundType] = useState('')
-
-  const handleFundName = (fundName: string) => {
-    setFundName(fundName)
-  }
-  const handleFundType = (fundType: string) => {
-    setFundType(fundType)
-  }
-  const openModal = () => {
-    setIsOpen(true)
-  }
-  const closeModal = () => {
-    setIsOpen(false)
-  }
-
-  const headers = ['No', 'Fund name', 'Unit Price', "24h%", "Market Cap"];
+  const headers = ["No", "Fund name", "Unit Price", "24h%", "Market Cap"];
 
   const data = [
     {
       No: 1,
-      name: 'Unguka fund',
+      name: "Unguka fund",
       unitPrice: 3000,
       change: 3.3,
-      marketCap: 345455656.34
+      marketCap: 345455656.34,
     },
     {
       No: 2,
-      name: 'Terimbere fund',
+      name: "Terimbere fund",
       fund: 3000,
       change: 3.3,
-      marketCap: 345455656.34
+      marketCap: 345455656.34,
     },
     {
       No: 3,
-      name: 'Umwalimu fund',
+      name: "Umwalimu fund",
       fund: 3000,
       change: 3.3,
-      marketCap: 345455656.34
+      marketCap: 345455656.34,
     },
     {
       No: 4,
-      name: 'Ejo heza fund',
+      name: "Ejo heza fund",
       fund: 3000,
       change: 3.3,
-      marketCap: 345455656.34
+      marketCap: 345455656.34,
     },
     {
       No: 5,
-      name: 'Ejo heza fund',
+      name: "Ejo heza fund",
       fund: 3000,
       change: 3.3,
-      marketCap: 345455656.34
+      marketCap: 345455656.34,
     },
     {
       No: 6,
-      name: 'Bk fund',
+      name: "Bk fund",
       fund: 3000,
       change: -3.3,
-      marketCap: 345455656.34
+      marketCap: 345455656.34,
     },
     {
       No: 7,
-      name: 'Equity fund',
+      name: "Equity fund",
       fund: 3000,
       change: 3.3,
-      marketCap: 345455656.34
-    }
-    ,
+      marketCap: 345455656.34,
+    },
     {
       No: 8,
-      name: 'I&M fund',
+      name: "I&M fund",
       fund: 3000,
       change: 3.3,
-      marketCap: 345455656.34
-    }
-    ,
+      marketCap: 345455656.34,
+    },
     {
       No: 9,
-      name: 'KCB fund',
+      name: "KCB fund",
       fund: 3000,
       change: 3.3,
-      marketCap: 345455656.34
-    }
-    ,
+      marketCap: 345455656.34,
+    },
     {
       No: 10,
-      name: 'Access fund',
+      name: "Access fund",
       fund: 3000,
       change: 3.3,
-      marketCap: 345455656.34
-    }
-    ,
+      marketCap: 345455656.34,
+    },
     {
       No: 11,
-      name: 'RIM fund',
+      name: "RIM fund",
       fund: 3000,
       change: 3.3,
-      marketCap: 345455656.34
+      marketCap: 345455656.34,
     },
-
   ];
-  const fundTypes: string[] = [
-    "Bk fund",
-    "Equity Fund"
-  ]
+
   return (
-        <div className="bg-[#eaeaed] min-h-[87vh] ">
-   <div className=" flex flex-col justify-center lg:mx-[9rem]">
+    <div className="bg-[#eaeaed] min-h-[87vh] ">
+      <div className=" flex flex-col justify-center lg:mx-[9rem]">
         <p className="text-[#475569] font-bold text-4xl py-4">Funds</p>
         <div className="flex flex-grow min-h-[66vh]">
           <Table
@@ -140,48 +114,16 @@ export default function Funds() {
             title="Funds types"
             buttonText="Create fund"
             buttonStyling="bg-[#002674] text-white py-2 px-4 mt-2 ml-4 rounded-lg"
-            buttonOnClick={openModal}
+            buttonOnClick={() => {}}
             marketCapIndex={4}
             changeIndex={3}
-          itemsPerPage={7} 
-          idIndex={0}
-          isTableRowLink={true}
-          displayButton={true}
+            itemsPerPage={7}
+            idIndex={0}
+            isTableRowLink={true}
+            displayButton={true}
           />
         </div>
-        <Modal isOpen={isOpen} onClose={closeModal} >
-          <div className='bg-white rounded-lg  h-1/1 px-10 py-5'>
-            <p className="px-8 text-[#475569] font-semibold">Create Fund</p>
-            <div className='py-3'>
-              <InputField
-                value={fundName}
-                placeholder='Enter Fund name'
-                required={false}
-                type='text'
-                className='text-xs'
-                label='Fund name'
-                onChange={handleFundName}
-                {...inputFieldStylingProps}
-              />
-            </div>
-            <div className='py-3'>
-              <SelectBox
-                value={fundType}
-                required={false}
-                values={fundTypes}
-                className='text-xs'
-                label='Fund type'
-                onChange={handleFundType}
-                {...inputFieldStylingProps}
-              />
-            </div>
-            <div className="flex justify-end px-8 ">
-              <Button styling='bg-[#002674] text-white py-2 px-4 mt-2  rounded-lg ' value='Create fund' />
-              <Button styling='bg-[#F0F4F8] text-[#475569] py-2 px-4 mt-2 ml-4 rounded-lg ' value='Cancel' />
-            </div>
-          </div>
-        </Modal>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
